afedf16644669c53a482c1b83178f1c0457b05f8
Gennadii <>
2024-03-13 18:07:30 +0800

    Setup project

---
+++ Source/TPSMultiplayer/TPSMultiplayerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSMultiplayerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATPSMultiplayerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATPSMultiplayerGameMode();
+};
+
+
+
