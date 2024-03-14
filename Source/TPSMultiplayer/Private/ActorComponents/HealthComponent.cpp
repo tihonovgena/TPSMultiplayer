503f622eeab4ed9cd7a9cc24058146b874aaa85b
Gennadii <>
2024-03-14 21:22:34 +0800

    Add health component

---
+++ Source/TPSMultiplayer/Private/ActorComponents/HealthComponent.cpp
@@ -0,0 +1,53 @@
+// TPSMultiplayer, Tikhonov Gennadii, All Rights Reserved
+
+
+#include "ActorComponents/HealthComponent.h"
+#include "Net/UnrealNetwork.h"
+
+DEFINE_LOG_CATEGORY(HealthComponent)
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	
+	Health = DefaultHealth;
+
+}
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ComponentOwner = GetOwner();
+	check(ComponentOwner)
+	ComponentOwner->OnTakeAnyDamage.AddUniqueDynamic(this, &UHealthComponent::OnTakeAnyDamage);
+	
+}
+
+void UHealthComponent::OnTakeAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType,
+									   AController* InstigatedBy, AActor* DamageCauser)
+{
+	Health = FMath::Clamp(Health - Damage, 0.f , DefaultHealth);
+	UE_LOG(HealthComponent, Display, TEXT("%s take %f damage"), *ComponentOwner->GetName(), Damage);
+}
+
+void UHealthComponent::OnRep_Health()
+{
+	
+	OnHealthChanged.Broadcast(Health);
+
+	UE_LOG(HealthComponent, Display, TEXT("New health: %f"), Health);
+
+	if (Health == 0)
+	{
+		OnDeath.Broadcast();
+	}
+	
+	
+}
+
+void UHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UHealthComponent, Health);
+}
---
+++ Source/TPSMultiplayer/Public/ActorComponents/HealthComponent.h
@@ -0,0 +1,56 @@
+// TPSMultiplayer, Tikhonov Gennadii, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(HealthComponent, Display, All);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, Health);
+
+UCLASS( ClassGroup=(HealthComponent), meta=(BlueprintSpawnableComponent) )
+class TPSMULTIPLAYER_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	
+	UHealthComponent();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Health")
+	float GetHealth(){return Health;}
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Health")
+	float GetDefaultHealth(){return DefaultHealth;}
+
+	UPROPERTY(BlueprintAssignable, Category = "Death")
+	FOnDeathSignature OnDeath;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FOnHealthChangedSignature OnHealthChanged;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void OnTakeAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
+	
+	UPROPERTY(EditDefaultsOnly, Category="Health", meta=(ClampMin = "0"))
+	float DefaultHealth = 100.f;
+
+	UPROPERTY(Replicated)
+	float Health;
+
+	UFUNCTION()
+	void OnRep_Health();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY()
+	TObjectPtr<AActor> ComponentOwner;
+		
+};
